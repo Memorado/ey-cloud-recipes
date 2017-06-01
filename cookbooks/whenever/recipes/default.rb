e74641d55bc289a43ae336cba246af90f3f8f766
Anton Safonov <maks2109@gmail.com>
2017-06-01 17:30:04 +0200

    checking server name only if its utility server, fix for staging since its solo type

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -2,27 +2,34 @@
 # Cookbook Name:: whenever
 # Recipe:: default
 #
+
+def apply_recipes
+  # Set your application name here
+  appname = "jobz_backend"
+
+  # be sure to replace "app_name" with the name of your application.
+  local_user = node[:users].first
+  execute "whenever" do
+    cwd "/data/#{appname}/current"
+    user local_user[:username]
+    command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
+    action :run
+  end
+
+  ey_cloud_report "whenever" do
+    message "whenever recipe complete"
+  end
+end
+
 ey_cloud_report "whenever" do
   message "starting whenever recipe"
 end
 
-# Set your application name here
-appname = "jobz_backend"
-
-if ['solo', 'util'].include?(node[:instance_role])
+# if ['solo', 'util'].include?(node[:instance_role])
+if ['solo'].include?(node[:instance_role])
+  apply_recipes
+elsif ['util'].include?(node[:instance_role])
   if ['utility_1'].include?(node[:instance_name])
-
-    # be sure to replace "app_name" with the name of your application.
-    local_user = node[:users].first
-    execute "whenever" do
-      cwd "/data/#{appname}/current"
-      user local_user[:username]
-      command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
-      action :run
-    end
-
-    ey_cloud_report "whenever" do
-      message "whenever recipe complete"
-    end
+    apply_recipes
   end
 end
