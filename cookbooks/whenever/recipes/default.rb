80b9e6d637d5e087c0f1c65dcfa2fb0c1a7f759b
Anton Safonov <maks2109@gmail.com>
2017-06-01 17:04:35 +0200

    populating crontab only on utility_1 server

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -10,17 +10,19 @@ end
 appname = "jobz_backend"
 
 if ['solo', 'util'].include?(node[:instance_role])
+  if instance_named?('utility_1')
 
-  # be sure to replace "app_name" with the name of your application.
-  local_user = node[:users].first
-  execute "whenever" do
-    cwd "/data/#{appname}/current"
-    user local_user[:username]
-    command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
-    action :run
-  end
+    # be sure to replace "app_name" with the name of your application.
+    local_user = node[:users].first
+    execute "whenever" do
+      cwd "/data/#{appname}/current"
+      user local_user[:username]
+      command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
+      action :run
+    end
 
-  ey_cloud_report "whenever" do
-    message "whenever recipe complete"
+    ey_cloud_report "whenever" do
+      message "whenever recipe complete"
+    end
   end
 end
